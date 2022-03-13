6683bb6c4ba9796cbf83339f833e089e47c03183
Evan Ray <elray@umass.edu>
2022-03-13 18:43:00 -0400

    predict method for kcqe

---
+++ jacques/kcqe.py
@@ -0,0 +1,87 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+from . import kernels
+
+
+class KCQE():
+    def __init__(self, x_kernel = 'gaussian_diag', p=1) -> None:
+        self.x_kernel = x_kernel
+        self.p = p
+        if x_kernel == 'gaussian_diag':
+            self.n_param = p + 1
+        elif x_kernel == 'gaussian_full':
+            self.n_param = p * (p - 1) / 2 + 1
+        else:
+            raise ValueError("x_kernel must be 'gaussian_diag' or 'gaussian_full'")
+        
+        # TODO: uncomment once KCQE extends jacques base class
+        # super(KCQE, self).__init__()
+    
+    
+    def unpack_param_vec(self, param_vec):
+        """
+        Unpack vector of parameters into a dictionary
+        
+        Inputs
+        ------
+        param_vec: tensor of length `(self.n_param,)`
+        
+        Returns
+        -------
+        dictionary with entries `x_bw_raw` and `y_bw_raw`
+        
+        Raises
+        ------
+        ValueError if len(param_vec) != self.n_param
+        """
+        if len(param_vec) != self.n_param:
+            raise ValueError("Require len(param_vec) == self.n_param")
+        
+        return {
+            'x_bw_raw': param_vec[:-1],
+            'y_bw_raw': param_vec[-1]
+        }
+    
+    
+    def predict(self, param_vec, x_train, y_train, x_test, tau):
+        """
+        Generate quantile predictions from a KCQE model
+        
+        Inputs
+        ------
+        x_train: tensor of shape `(batch_shape) + (n_train, p)` with training
+            set features
+        y_train: tensor of shape `(batch_shape) + (n_train,)` with training
+            set response values
+        x_test: tensor of shape `(batch_shape) + (n_test, p)` with test set
+            features
+        tau: tensor of length `k` with probability levels at which to extract
+            quantile estimates
+        
+        Returns
+        -------
+        tensor of shape `(batch_shape) + (n_test, k)` with test set quantile
+            estimates at each quantile level
+        
+        Raises
+        ------
+        ValueError if the last dimension of `x_train` and `x_test` don't match
+        `self.p`
+        """
+        if x_train.shape.as_list()[-1] != self.p or x_test.shape.as_list()[-1] != self.p:
+            raise ValueError("x_train and x_test must contain self.p features in their last dimension")
+        
+        param_dict = self.unpack_param_vec(param_vec)
+        
+        # w_train is a tensor of shape `(batch_shape) + (n_test, n_train)`
+        w_train = kernels.kernel_weights(x1=x_test,
+                                         x2=x_train,
+                                         theta_b=param_dict['x_bw_raw'],
+                                         kernel=self.x_kernel)
+        
+        return kernels.kernel_quantile_fn(y=tf.expand_dims(y_train, -2), # will be broadcast to match shape of w_train
+                                          w=w_train,
+                                          tau=tau,
+                                          theta_b_raw=param_dict['y_bw_raw'])
---
+++ test/test_gaussian_kernel.py
@@ -0,0 +1,84 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+import unittest
+
+from jacques import kernels
+
+
+class Test_Gaussian_Kernel(unittest.TestCase):
+    def test_diff_x_pairs(self):
+        x1 = np.arange(2*3*10*5).reshape(2, 3, 10, 5)
+        x2 = np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000
+        x1_tf = tf.constant(x1)
+        x2_tf = tf.constant(x2)
+
+        expected_result = np.zeros((2, 3, 10, 4, 5))
+        for i in range(10):
+            for j in range(4):
+                expected_result[:, :, i, j, :] = x1[:, :, i, :] - x2[:, :, j, :]
+
+        actual_result = tf.expand_dims(x1_tf, -2) - tf.expand_dims(x2_tf, -3)
+
+        actual_result = kernels.diff_x_pairs(x1_tf, x2_tf)
+
+        self.assertTrue(np.all(actual_result.numpy() == expected_result))
+    
+    
+    def test_gaussian_full_kernel(self):
+        x1 = tf.constant(np.arange(2*3*10*5).reshape(2, 3, 10, 5), dtype='float64')
+        x2 = tf.constant(np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000, dtype='float64')
+
+        B_chol = tfp.math.fill_triangular(np.arange(15)/25000)
+        B = tf.matmul(B_chol, B_chol, transpose_b=True)
+
+        diffs = kernels.diff_x_pairs(x1, x2)
+
+        expected_result = np.zeros((2, 3, 10, 4))
+        for b1 in range(2):
+            for b2 in range(3):
+                for i in range(10):
+                    for j in range(4):
+                        expected_result[b1, b2, i, j] = tf.matmul(
+                            tf.matmul(tf.expand_dims(diffs[b1, b2, i, j], 0), B),
+                            tf.expand_dims(diffs[b1, b2, i, j], -1)
+                        )
+
+        expected_result = np.exp(-1.0 * expected_result)
+
+        actual_result = kernels.gaussian_kernel(x1, x2, B_chol)
+
+        self.assertTrue(np.all(np.abs(actual_result.numpy() - expected_result) < 1e-12))
+    
+    
+    def test_gaussian_full_weights(self):
+        x1 = tf.constant(np.arange(2*3*10*5).reshape(2, 3, 10, 5), dtype='float64')
+        x2 = tf.constant(np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000, dtype='float64')
+        B_chol = tfp.math.fill_triangular(np.arange(15)/25000)
+
+        kernel_val = kernels.gaussian_kernel(x1, x2, B_chol)
+        expected_weights = np.zeros((2, 3, 10, 4))
+        for b1 in range(2):
+            for b2 in range(3):
+                for i in range(10):
+                    temp = kernel_val.numpy()[b1, b2, i, :]
+                    expected_weights[b1, b2, i, :] = temp / np.sum(temp)
+                # for j in range(4):
+                #     temp = kernel_val.numpy()[b1, b2, :, j]
+                #     expected_weights[b1, b2, :, j] = temp / np.sum(temp)
+
+
+        actual_weights = kernels.kernel_weights(
+            x1, x2,
+            np.arange(15)/25000,
+            kernel = 'gaussian_full')
+
+        # actual matches expected
+        self.assertTrue(np.all(np.abs(actual_weights.numpy() - expected_weights) < 1e-12))
+
+        # within batches and x2 observations, weights sum to 1 across x1 observations
+        self.assertTrue(np.all(np.abs(tf.reduce_sum(actual_weights, axis=-1).numpy() - np.ones((2, 3, 10))) < 1e-12))
+
