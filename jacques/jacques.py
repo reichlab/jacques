8b4626fbba47e1a446b61cd397ee5559445d4030
Serena Yijin Wang <wang225y@mtholyoke.edu>
2022-03-21 14:08:01 -0400

    update documentation

--- jacques/jacques.py
+++ jacques/jacques.py
@@ -110,7 +110,7 @@ class jacques(abc.ABC):
         -------
         num_blocks: integer
             Total number of block could be created with given dataset
-        x_test: 3D tensor with shape (L, T, P = 1)
+        x_test: 2D tensor with shape (L, P = 1)
             Each value is test set feature for each location at forecast date.
         xval_batch_gen: generator
             A generator initialized with x_train_val and y_train_val, block_size and batch_size
