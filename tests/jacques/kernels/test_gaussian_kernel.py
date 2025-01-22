06c44a6fdfef62604e0875d109f42a278fe40387
benwrogers87 <35546741+benwrogers87@users.noreply.github.com>
2025-01-22 10:44:01 -0500

    switched gaussian_kernel tests to use  pytest

--- tests/jacques/kernels/test_gaussian_kernel.py
+++ tests/jacques/kernels/test_gaussian_kernel.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+import pytest
 
 import numpy as np
 import tensorflow as tf
@@ -8,55 +9,51 @@ import tensorflow_probability as tfp
 
 from jacques import kernels
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
 
-class Test_Gaussian_Kernel(unittest.TestCase):
-    def test_diff_x_pairs(self):
-        x1 = np.arange(2*3*10*5).reshape(2, 3, 10, 5)
-        x2 = np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000
-        x1_tf = tf.constant(x1)
-        x2_tf = tf.constant(x2)
+def test_diff_x_pairs():
+    x1 = np.arange(2*3*10*5).reshape(2, 3, 10, 5)
+    x2 = np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000
+    x1_tf = tf.constant(x1)
+    x2_tf = tf.constant(x2)
 
-        expected_result = np.zeros((2, 3, 10, 4, 5))
-        for i in range(10):
-            for j in range(4):
-                expected_result[:, :, i, j, :] = x1[:, :, i, :] - x2[:, :, j, :]
+    expected_result = np.zeros((2, 3, 10, 4, 5))
+    for i in range(10):
+        for j in range(4):
+            expected_result[:, :, i, j, :] = x1[:, :, i, :] - x2[:, :, j, :]
 
-        actual_result = tf.expand_dims(x1_tf, -2) - tf.expand_dims(x2_tf, -3)
+    actual_result = tf.expand_dims(x1_tf, -2) - tf.expand_dims(x2_tf, -3)
 
-        actual_result = kernels.diff_x_pairs(x1_tf, x2_tf)
+    actual_result = kernels.diff_x_pairs(x1_tf, x2_tf)
 
-        self.assertTrue(np.all(actual_result.numpy() == expected_result))
+    assert np.all(actual_result.numpy() == expected_result) 
+
+def test_gaussian_full_kernel():
+    x1 = tf.constant(np.arange(2*3*10*5).reshape(2, 3, 10, 5), dtype='float64')
+    x2 = tf.constant(np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000, dtype='float64')
+
+    B_chol = tfp.math.fill_triangular(np.arange(15)/25000)
+    B = tf.matmul(B_chol, B_chol, transpose_b=True)
+
+    diffs = kernels.diff_x_pairs(x1, x2)
+
+    expected_result = np.zeros((2, 3, 10, 4))
+    for b1 in range(2):
+        for b2 in range(3):
+            for i in range(10):
+                for j in range(4):
+                    expected_result[b1, b2, i, j] = tf.matmul(
+                        tf.matmul(tf.expand_dims(diffs[b1, b2, i, j], 0), B),
+                        tf.expand_dims(diffs[b1, b2, i, j], -1)
+                    )
+
+    expected_result = np.exp(-1.0 * expected_result)
+
+    actual_result = kernels.gaussian_kernel(x1, x2, B_chol)
+
+    assert(np.all(np.abs(actual_result.numpy() - expected_result) < 1e-12))
     
     
-    def test_gaussian_full_kernel(self):
-        x1 = tf.constant(np.arange(2*3*10*5).reshape(2, 3, 10, 5), dtype='float64')
-        x2 = tf.constant(np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000, dtype='float64')
-
-        B_chol = tfp.math.fill_triangular(np.arange(15)/25000)
-        B = tf.matmul(B_chol, B_chol, transpose_b=True)
-
-        diffs = kernels.diff_x_pairs(x1, x2)
-
-        expected_result = np.zeros((2, 3, 10, 4))
-        for b1 in range(2):
-            for b2 in range(3):
-                for i in range(10):
-                    for j in range(4):
-                        expected_result[b1, b2, i, j] = tf.matmul(
-                            tf.matmul(tf.expand_dims(diffs[b1, b2, i, j], 0), B),
-                            tf.expand_dims(diffs[b1, b2, i, j], -1)
-                        )
-
-        expected_result = np.exp(-1.0 * expected_result)
-
-        actual_result = kernels.gaussian_kernel(x1, x2, B_chol)
-
-        self.assertTrue(np.all(np.abs(actual_result.numpy() - expected_result) < 1e-12))
-    
-    
-    def test_gaussian_full_weights(self):
+def test_gaussian_full_weights():
         x1 = tf.constant(np.arange(2*3*10*5).reshape(2, 3, 10, 5), dtype='float64')
         x2 = tf.constant(np.arange(2*3*4*5).reshape(2, 3, 4, 5) + 1000, dtype='float64')
         theta_raw = np.arange(15) - 7.5
@@ -78,11 +75,7 @@ class Test_Gaussian_Kernel(unittest.TestCase):
             kernel = 'gaussian_full')
 
         # actual matches expected
-        self.assertTrue(np.all(np.abs(actual_weights.numpy() - expected_weights) < 1e-12))
+        assert(np.all(np.abs(actual_weights.numpy() - expected_weights) < 1e-12))
 
         # within batches and x2 observations, weights sum to 1 across x1 observations
-        self.assertTrue(np.all(np.abs(tf.reduce_sum(actual_weights, axis=-1).numpy() - np.ones((2, 3, 10))) < 1e-12))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert(np.all(np.abs(tf.reduce_sum(actual_weights, axis=-1).numpy() - np.ones((2, 3, 10))) < 1e-12))
