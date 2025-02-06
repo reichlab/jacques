f4ecd5c319efcd6d899522e459cc1733b7dab2df
benwrogers87 <35546741+benwrogers87@users.noreply.github.com>
2025-02-06 15:36:21 -0500

    masking fails when batchsize is larger than 1

--- src/jacques/jacques.py
+++ src/jacques/jacques.py
@@ -130,17 +130,6 @@ class jacques(abc.ABC):
             y_train = tf.reshape(y_train, [-1])
             y_train = tf.expand_dims(y_train, axis=0)
 
-            # Drop any entries with missing data in either target or features
-            mask = tf.reduce_all(~tf.math.is_nan(x_val), axis = -1) & ~tf.math.is_nan(y_val)
-
-            x_val = tf.boolean_mask(x_val, mask)
-            y_val = tf.boolean_mask(y_val, mask)
-
-            mask = tf.reduce_all(~tf.math.is_nan(x_train), axis = -1) & ~tf.math.is_nan(y_train)
-            x_train = tf.boolean_mask(x_train, mask)
-            y_train = tf.boolean_mask(y_train, mask)
-
-
             i += 1
 
             yield x_val, x_train, y_val, y_train
